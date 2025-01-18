f6b9dca3c7fb88a1c9fe0409d54dc5433cc54084
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2025-01-18 14:24:31 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Register from './pages/user/Register'
+import { Provider } from 'react-redux'
+import store from './store/store'
+import Home from './pages/home/Home'
+import Login from './pages/user/Login'
+import Product from './pages/product/Product'
+import SingleProduct from './pages/single-product/SingleProduct'
+
+function App() {
+
+  return (
+  <Provider store={store}>
+     <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/register' element={<Register />}/>
+    <Route path='/login' element={<Login />}/>
+    <Route path='/products' element={<Product />}/>
+    <Route path='/products/:id' element={<SingleProduct />}/>
+    
+    </Routes>
+    </BrowserRouter>
+  </Provider>
+  )
+}
+
+export default App

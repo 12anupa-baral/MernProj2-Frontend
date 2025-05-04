564a7d2484d79c124261f1c31de1874b67a9d149
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2025-05-04 07:43:17 +0545

    token fetched dynamically

--- src/http/index.ts
+++ src/http/index.ts
@@ -8,13 +8,32 @@ const API = axios.create({
   },
 });
 
+// const APIWITHTOKEN = axios.create({
+//   baseURL: "http://localhost:3001/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//     Authorization: localStorage.getItem("tokenHoYo"),
+//   },
+// });
+
+// export { API, APIWITHTOKEN };
+
+
 const APIWITHTOKEN = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://your-ngrok-url.ngrok-free.app/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    Authorization: localStorage.getItem("tokenHoYo"),
   },
 });
 
-export { API, APIWITHTOKEN };
+APIWITHTOKEN.interceptors.request.use((config) => {
+  const token = localStorage.getItem("tokenHoYo");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+ export { API, APIWITHTOKEN };
